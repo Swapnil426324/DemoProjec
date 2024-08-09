6e0602e32181dc3cfa10885d3d7ba4d6086a650c
Swapnil426324 <alaskarswapnil1998@gmail.com>
2024-08-09 13:04:09 +0530

    Update ADemo.java
    new changes by master

--- Demo/src/com/demo/ADemo.java
+++ Demo/src/com/demo/ADemo.java
@@ -3,27 +3,27 @@ package com.demo;
 public class ADemo {
 
 	public static void main(String[] args) {
-		System.out.println("hii users");
+	System.out.println("hii users");
                
-		System.out.println("new changes added in master");
+	System.out.println("new changes added in master");
 
         // how to get conflict by master? 
-	    System.out.println("changesh in my master");
+	System.out.println("changesh in my master");
 	
         // how to get conflict by master? 
-	    System.out.println("changesh in my master");
+	System.out.println("changesh in my master");
 
         // how to get conflict by local? 
-		System.out.println("changesh in my local");
+	System.out.println("changesh in my local");
 		
-		// another way
-		System.out.println("commit by local");
+	// another way
+	System.out.println("commit by local");
 		
         // how to get conflict by local? 
-		 System.out.println("changesh in my local");
+        System.out.println("changesh in my local");
 		
         // another way
-		System.out.println("commit by master before local");
+	System.out.println("commit by master before local");
 	       
 	}
 
