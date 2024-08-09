098d5eb44568ff31de1587cc275d3623fee1f9f9
Swapnil426324 <alaskarswapnil1998@gmail.com>
2024-08-09 12:15:56 +0530

    Update ADemo.java

--- Demo/src/com/demo/ADemo.java
+++ Demo/src/com/demo/ADemo.java
@@ -4,9 +4,9 @@ public class ADemo {
 
 	public static void main(String[] args) {
 		System.out.println("hii users");
-                void m2() {
-		System.out.println("method Of B2");
-	       }
+               
+		System.out.println("new changes added in master");
+	       
 	}
 
 }
