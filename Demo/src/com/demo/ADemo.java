ea1e60eb23b74365ff6e1703071ea86da99d7d4a
SAGAR ALASKAR <SAGAR ALASKAR@DESKTOP-NL9CC06>
2024-08-09 13:06:00 +0530

    process done changes

--- Demo/src/com/demo/ADemo.java
+++ Demo/src/com/demo/ADemo.java
@@ -3,28 +3,29 @@ package com.demo;
 public class ADemo {
 
 	public static void main(String[] args) {
-	System.out.println("hii users");
-               
-	System.out.println("new changes added in master");
+		System.out.println("hii users");
 
-        // how to get conflict by master? 
-	System.out.println("changesh in my master");
-	
-        // how to get conflict by master? 
-	System.out.println("changesh in my master");
+		System.out.println("new changes added in master");
 
-        // how to get conflict by local? 
-	System.out.println("changesh in my local");
-		
-	// another way
-	System.out.println("commit by local");
-		
-        // how to get conflict by local? 
-        System.out.println("changesh in my local");
-		
-        // another way
-	System.out.println("commit by master before local");
-	       
+		// how to get conflict by master?
+		System.out.println("changesh in my master");
+
+		// how to get conflict by master?
+		System.out.println("changesh in my master");
+
+		// how to get conflict by local?
+		System.out.println("changesh in my local");
+
+		// another way
+		System.out.println("commit by local");
+
+		// how to get conflict by local?
+		System.out.println("changesh in my local");
+
+		// another way
+		System.out.println("commit by master before local");
+		System.out.println("process done");
+
 	}
 
 }
